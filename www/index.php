91f8f4c92e48625e449ab30939916df7aec3458a
Aniello <99719850+iNiello@users.noreply.github.com>
2022-03-23 17:02:41 +0100

    fix: WIP #6

--- www/index.php
+++ www/index.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>jeu</title>
 </head>
 <body>
-    
+    <div align="center">HERO</div>
+    <form action="index.php" method="post">
+        <label for="">point d'expérience</label>
+        <input type="text" placeholder="commence à 10 points" name="experience">
+        <label for="">point de vie</label>
+        <input type="text" placeholder="commence à 10 points" name="vie">
+        <label for="">point de force</label>
+        <input type="text" placeholder="commence à 2 points (1+1arme)" name="experience">
+        <label for="">point d'endurence</label>
+        <input type="text" placeholder="commence à 1 points" name="experience">
+        <label for="">sac d'armes</label>
+        <input type="text" placeholder="commence à 1 arme" name="experience">
+        <label for="">sous</label>
+        <input type="text" placeholder="commence à 20 €" name="experience">
+
+    </form>
 </body>
 </html>
