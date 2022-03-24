d35d47ea31e37e8fcf49786672e690da2227526b
Aniello <99719850+iNiello@users.noreply.github.com>
2022-03-24 17:04:16 +0100

    fix: WIP #6
    Add dropdown Arme
    Add Wireframe Switch case

--- www/index.php
+++ www/index.php
@@ -1,3 +1,12 @@
+<?php 
+
+      include "isset.php";
+    /*include "hero.php";
+      include "marchant.php";
+      include "monstre.php";
+    */
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -29,12 +38,40 @@
         </div>
         <div class="form-floating col-2">
             <input type="texte" class="form-control" id="arme" placeholder="commence à 1 arme" name="arme">
-            <label for="floatingInput">Sac d'armes</label>
+            <label for="floatingInput">Armes</label>
         </div>
+        
+        
+        <div class="col-2">
+            <label for="disabledSelect" class="form-label">Sac d'armes</label>
+            <select id="disabledSelect" class="form-select">
+                <option>Arme 1000</option>
+                <option>Arme 3000</option>
+                <option>Arme 5000</option>
+            </select>
+        </div>
+
+
         <div class="form-floating col-2">
             <input type="texte" class="form-control" id="sous" placeholder="commence à 20€" name="sous">
             <label for="floatingInput">Sous</label>
         </div>
+        <button type="submit" class="col-2 btn btn btn-outline-primary btn-lg" name="start">
+            start
+        </button>
+
     </form>
+    
+    <div class="dropdown">
+            <button class="col-2 btn btn-secondary dropdown-toggle" type="button" id="sac-armes" name="sac-armes">jo
+            </button>
+            <!-- <label for="floatingInput">Sac d'armes</label> -->
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    
 </body>
 </html>
