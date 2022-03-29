4424c253b2f05a5c6d6bb82c1933d8b2248185fd
Aniello <99719850+iNiello@users.noreply.github.com>
2022-03-29 15:13:09 +0200

    Update arme.php

--- www/arme.php
+++ www/arme.php
@@ -1,10 +1,6 @@
 <?php
 //classe arm
-<<<<<<< Updated upstream:www/arme.php
 class Arme {
-=======
-class Arme{
->>>>>>> Stashed changes:www/Arm.php
     private $_nom;
     private $_force;
     private $_endurance;
